4a4a9e35ff058b71942ce9a8bea55f714b1a08d9
yousefm3 <96112309+yousefm3@users.noreply.github.com>
2023-05-18 01:08:43 +0300

    ...

--- books recommendation/top50.py
+++ books recommendation/top50.py
@@ -5,5 +5,17 @@ ratings_data = pd.read_csv('Ratings.csv')
 isbn_counts = ratings_data['ISBN'].value_counts()
 top_50_isbn = isbn_counts.head(50).index
 
-book_names = books_data.loc[books_data['ISBN'].isin(top_50_isbn), 'Book-Title'].values
-print(book_names)
+# Filter books_data based on the top 50 ISBN values and retrieve the "Book-Title" and "Image-URL-M" columns
+filtered_books_data = books_data.loc[books_data['ISBN'].isin(top_50_isbn), ['Book-Title', 'Image-URL-M']]
+
+# Create an empty array to store book and image pairs
+book_image_array = []
+
+# Iterate over the filtered_books_data and add each book with its image URL to the array
+for index, row in filtered_books_data.iterrows():
+    book_name = row['Book-Title']
+    image_url = row['Image-URL-M']
+    book_image_array.append((book_name, image_url))
+
+# Print the book and image pairs in the array
+print(book_image_array)
