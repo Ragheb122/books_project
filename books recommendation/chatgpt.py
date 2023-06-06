9bd5e675997fba2ce9c65ee7e0b3f7a1ae405e0e
raghebghazi <raghep-11@hotmail.com>
2023-06-06 17:53:50 +0300

    ..

--- books recommendation/chatgpt.py
+++ books recommendation/chatgpt.py
@@ -1,10 +1,25 @@
 from flask import Flask, request, jsonify, render_template
 import openai
+import requests
 from flask_cors import CORS
-
+import json
 
 app = Flask(__name__)
 CORS(app)
+
+
+response = requests.get('http://localhost:1338/posts')
+
+if response.status_code == 200:
+    data = response.json()
+    arr = []
+    if data.get('code') == 200:
+        # Your code here if the code is 200
+        for post in data['Data']:
+            arr.append((post['title'], post['id']))
+    arr = [('Wild Animus',1), ('Timeline',2), ('To Kill a Mockingbird',3),
+           ('Harry Potter and the Sorcerers Stone (Harry Potter (Paperback))',4)
+]
 
 # Set up OpenAI API credentials
 openai.api_key = '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'
@@ -23,17 +38,17 @@ def generate_description():
     # Generating chat response
     response = openai.Completion.create(
         engine='text-davinci-003',
-        prompt='Give me a name of a famous book about:' + user_input + '\nwith a short description and a link to it in amazon.',
+        prompt=f'Give me a name of the most relevant book from this array: {arr} according to this description: {user_input}.in addition write this link http://localhost:3001/book/34',
         max_tokens=200,
         temperature=0.7,
         n=1,
         stop=None
     )
-
     # Extracting the description from the chat response
     description = response.choices[0].text.strip()
-
+    print(description)
     # Return the generated description as a JSON response
+    print(jsonify({'description': description}))
     return jsonify({'description': description})
 
 if __name__ == '__main__':
